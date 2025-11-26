7774461d9f99fc58e16ff75514b2bf8228487a26
max029113 nauduik <maks.naud@gmail.com>
2025-11-26 18:05:42 +0200

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 
 FILE_PATH_1 = 'Closed_window.csv'
 FILE_PATH_2 = 'Open_window.csv'
@@ -8,14 +9,26 @@ def load_and_analyze(file_path, location_name):
     try:
         df = pd.read_csv(file_path, sep=',')
         
+        if isinstance(df['Time'][0], str):
+            df['Time'] = pd.to_datetime(df['Time'])
+            time_difference = df['Time'].max() - df['Time'].min()
+            duration = time_difference.total_seconds()
+        else:
+            duration = df['Time'].max() - df['Time'].min()
+
         avg_noise = df['dBa'].mean()
         max_noise = df['dBa'].max()
         min_noise = df['dBa'].min()
-        
+        variation = df['dBa'].var()
+
+                
         print(f"--- Analysis: {location_name} ---")
+        print(f" Duration: {duration} seconds")
         print(f" Average Noise Level: {avg_noise:.2f} dBA")
         print(f" Maximum Noise Level: {max_noise} dBA")
         print(f" Minimum Noise Level: {min_noise} dBA")
+        print(f" Variation: {variation} ")
+
         print("---------------------------------")
         
         df = df.rename(columns={'dBa': location_name})
